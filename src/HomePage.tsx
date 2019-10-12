d49bd088eb1a58d4869be8943a51033114eaa897
Travis Pomeroy <travis.pomeroy84@gmail.com>
2019-10-12 11:50:42 -0400

    Added a homepage.

---
+++ src/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight Administration</h1>
+      <p>React, Flux, and React Router for ultra-responsive web apps.</p>
+    </div>
+  );
+};
+
+export default HomePage;
