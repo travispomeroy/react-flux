2c0e02064855663dca19c3b2b886ac3671817654
Travis Pomeroy <travis.pomeroy84@gmail.com>
2019-10-12 21:17:53 -0400

    Made existing courses editable.

--- src/CourseForm.tsx
+++ src/CourseForm.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Course} from "./CoursesPage";
 import TextInput from "./TextInput";
+import {FormErrors} from "./ManageCoursePage";
 
 interface CourseFormProp {
     course: Course;
     handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
     handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+    formErrorMessages: FormErrors;
 }
 
 const CourseForm: React.FC<CourseFormProp> = props => {
@@ -13,7 +15,13 @@ const CourseForm: React.FC<CourseFormProp> = props => {
 
     return (
         <form onSubmit={props.handleSubmit}>
-            <TextInput id="title" label="Title" handleChange={props.handleChange} value={props.course.title} name="title" />
+            <TextInput id="title"
+                       label="Title"
+                       handleChange={props.handleChange}
+                       value={props.course.title}
+                       name="title"
+                       error={props.formErrorMessages.titleErrorMessage}
+            />
 
             <div className="form-group">
                 <label htmlFor="author">Author</label>
@@ -24,9 +32,19 @@ const CourseForm: React.FC<CourseFormProp> = props => {
                         <option value="2">Scott Allen</option>
                     </select>
                 </div>
+                {props.formErrorMessages.authorIdErrorMessage && (
+                    <div className="alert alert-danger">{props.formErrorMessages.authorIdErrorMessage}
+                    </div>
+                )}
             </div>
 
-            <TextInput id="category" label="Category" handleChange={props.handleChange} name="category" value={course.category}/>
+            <TextInput id="category"
+                       label="Category"
+                       handleChange={props.handleChange}
+                       name="category"
+                       value={course.category}
+                       error={props.formErrorMessages.categoryErrorMessage}
+            />
 
             <input type="submit" value="Save" className="btn btn-primary" />
         </form>
--- src/ManageCoursePage.tsx
+++ src/ManageCoursePage.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {RouteComponentProps} from "react-router-dom";
 import CourseForm from "./CourseForm";
 import {Course} from "./CoursesPage";
-import {saveCourse} from "./api/courseApi";
+import {getCourseBySlug, saveCourse} from "./api/courseApi";
 import {toast} from "react-toastify";
 
 type ManageCoursePageProps = RouteComponentProps<{ slug: string }>;
 
+export interface FormErrors {
+    titleErrorMessage?: string;
+    authorIdErrorMessage?: string;
+    categoryErrorMessage?: string;
+}
+
 const ManageCoursePage: React.FC<ManageCoursePageProps> = props => {
+    const [errors, setErrors] = React.useState<FormErrors>({});
     const [course, setCourse] = React.useState<Course>({
         id: "",
         slug: "",
@@ -16,22 +23,42 @@ const ManageCoursePage: React.FC<ManageCoursePageProps> = props => {
         category: ""
     });
 
+    useEffect(() => {
+       const slug = props.match.params.slug;
+       if (slug) {
+           getCourseBySlug(slug).then(response => setCourse(response));
+       }
+    }, [props.match.params.slug]);
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>): void => {
         setCourse({...course, [e.currentTarget.name]: e.currentTarget.value});
     };
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
         e.preventDefault();
+        if (!formIsValid()) return;
         saveCourse(course).then(() => {
-            props.history.push("/courses")
+            props.history.push("/courses");
             toast.success("Course saved.");
         });
+    };
+
+    const formIsValid = (): boolean => {
+        const errors:FormErrors = {};
+
+        if (!course.title) errors.titleErrorMessage = "Title is required";
+        if (!course.authorId) errors.authorIdErrorMessage = "Author ID is required";
+        if (!course.category) errors.categoryErrorMessage = "Category is required";
+
+        setErrors(errors);
+
+        return Object.keys(errors).length === 0;
     };
 
     return (
         <>
             <h2>Manage Course</h2>
-            <CourseForm course={course} handleChange={handleChange} handleSubmit={handleSubmit} />
+            <CourseForm formErrorMessages={errors} course={course} handleChange={handleChange} handleSubmit={handleSubmit} />
         </>
     );
 };
