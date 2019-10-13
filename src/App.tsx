a5ac958996f6b26abf61003e6bbaea141be941a0
Travis Pomeroy <travis.pomeroy84@gmail.com>
2019-10-12 22:55:44 -0400

    Pushed the creation of the store and actions to the higher level component.

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,8 @@ const App: React.FC = () => {
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
-        <Route path="/course/:slug" component={ManageCoursePage} />
-        <Route path="/course" component={ManageCoursePage} />
+        <Route path="/course/:slug" render={(props) => <ManageCoursePage {...props} courseActions={courseActions} courseStore={courseStore} />} />
+        <Route path="/course" render={(props) => <ManageCoursePage {...props} courseActions={courseActions} courseStore={courseStore} />} />
         <Redirect from="/about-page" to="about" />
         <Route component={PageNoteFound} />
       </Switch>
